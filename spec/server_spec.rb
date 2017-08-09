bec6a83e8327f27bde82101bc5eef41211bc13f7
Jakob Holderbaum <jakob@holderbaum.io>
2017-08-09 21:23:31 +0200

    Add www role with simple HTTP test

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'net/http'
 
 describe port(22) do
   it { should be_listening }
@@ -6,4 +7,15 @@ end
 
 describe service('ufw') do
   it { should be_running }
+end
+
+describe 'www' do
+  describe port(80) do
+    it { should be_listening }
+  end
+
+  it 'should serve content' do
+    response = Net::HTTP.get_response(external_ip, '/index.html')
+    expect(response.code).to eq('200')
+  end
 end
