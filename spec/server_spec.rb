31d65a8bd617aba6da83728008975b273d1aeabc
Jakob Holderbaum <jakob@holderbaum.io>
2017-08-18 14:53:08 +0200

    Install certbot and allow Nginx webroot access

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -18,4 +18,12 @@ describe 'www' do
     response = Net::HTTP.get_response(external_ip, '/index.html')
     expect(response.code).to eq('200')
   end
+
+  it 'should enable certbot challenges' do
+    challenge_key = 'YYYCT5-SxWTOd1ZJCI-jCEFwdAfavJublNB--RR0kac'
+    path = '/.well-known/acme-challenge/' + challenge_key
+
+    response = Net::HTTP.get_response(external_ip, path)
+    expect(response.body).to eq('test-challenge')
+  end
 end
