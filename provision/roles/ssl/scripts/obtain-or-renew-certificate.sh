a54824d1380f537d577eac2b6d2fb2e29b1c5233
Jakob Holderbaum <jakob@holderbaum.io>
2017-09-01 16:22:35 +0200

    Use same script for generating real/fake certificates

--- provision/roles/ssl/scripts/obtain-or-renew-certificate.sh
+++ provision/roles/ssl/scripts/obtain-or-renew-certificate.sh
@@ -6,7 +6,11 @@ webroot=$1
 target_dir=$2
 mail=$3
 domain=$4
+env=$5
 
+certbot_path="/etc/letsencrypt/live/$domain"
+
+if [ "$env" == "production" ]; then
 certbot \
   certonly \
   --test-cert \
@@ -16,9 +20,20 @@ certbot \
   --webroot-path "$webroot" \
   -m "$mail" \
   -d "$domain"
+else
+  mkdir -p "$certbot_path"
+
+  openssl req -x509 -newkey rsa:2048 \
+    -keyout "${certbot_path}/privkey.pem" \
+    -out "${certbot_path}/fullchain.pem" \
+    -nodes \
+    -days 365 \
+    -subj "/C=DE/L=Test/OU=Test/CN=${domain}"
+fi
+
 
 cp -Lr \
-  "/etc/letsencrypt/live/$domain" \
+  "$certbot_path" \
   "$target_dir"
 
 cat "$target_dir/$domain/privkey.pem" "$target_dir/$domain/fullchain.pem" >"$target_dir/$domain/cert.pem"
