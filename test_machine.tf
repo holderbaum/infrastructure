8bbc878522a62188f60e67c9f5d269067e69ead2
Jakob Holderbaum <jakob@holderbaum.io>
2017-09-05 22:05:28 +0200

    Use smaller machine for testing

--- test_machine.tf
+++ test_machine.tf
@@ -13,7 +13,7 @@ resource "digitalocean_droplet" "test_machine" {
   image = "ubuntu-16-04-x64"
   name = "turing.example.org"
   region = "fra1"
-  size = "8gb"
+  size = "4gb"
   ssh_keys = [ "${digitalocean_ssh_key.default.id}" ]
 }
 
