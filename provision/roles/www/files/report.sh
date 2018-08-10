fc84d08f5197e9d8da1197678fefc6dbd3208ea4
Jakob Holderbaum <jakob@holderbaum.io>
2018-08-10 14:47:53 +0200

    Add goaccess reports to every static page

---
+++ provision/roles/www/files/report.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+for log_file in /data/static_sites/*/access.log;
+do
+  if [ "$(cat "$log_file" |wc -l)" -gt 0 ];
+  then
+    report_dir="$(dirname "$log_file")/report"
+    name="$(basename "$(dirname "$log_file")")"
+    user="deploy-$name"
+    group="deploy-$name"
+    mkdir -p "$report_dir"
+    goaccess -a "$log_file" -o "$report_dir/index.html" --log-format=COMBINED
+    chown -R "${user}:${group}" "$report_dir"
+  else
+    echo "Skipping empty $log_file"
+  fi
+done
